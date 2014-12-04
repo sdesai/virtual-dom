9f813ee6957e20593b787b4f7a645337b4401d30
Satyen Desai <satyend@netflix.com>
2014-12-04 09:51:17 -0800

    Starting to clean stuff up

---
+++ Component.js
@@ -0,0 +1,114 @@
+var Rx = require('rx');
+
+var Observable = Rx.Observable;
+var ObservableOfSync = Observable.ofWithScheduler.bind(Observable, Rx.Scheduler.immediate);
+
+function toArray(arrayLike) {
+    return Array.prototype.slice.call(arrayLike, 0);
+}
+
+function Component(state) {
+    this.states = new Rx.Subject();
+    this.mounted = new Rx.Subject();
+
+    this.setState(state);
+}
+
+Component._cache = {};
+
+Component.create = function(proto) {
+
+    var component = function(state) {
+
+        var comp;
+
+        if (state && state.guid) {
+            comp = Component._cache[state.guid];
+        }
+
+        if (!comp) {
+
+            if (!(this instanceof component)) {
+                comp = new component(state);
+            } else {
+                Component.call(this, state);
+
+                if (state) {
+                    Component._cache[state.guid] = this;
+                }
+
+                comp = this;
+            }
+
+        } else {
+            comp.setState(state);
+        }
+
+        return comp;
+    };
+
+    component.prototype = Object.create(Component.prototype);
+    component.prototype.constructor = component;
+
+    for (var o in proto) {
+        component.prototype[o] = proto[o];
+    }
+
+    return component;
+}
+
+Component.prototype = Object.create(Observable.prototype);
+Component.prototype.constructor = Component;
+Component.prototype.type = "Widget";
+
+Component.prototype.render = function(state) {return null};
+
+Component.prototype.setState = function(state) {
+    this.states.onNext(state);
+};
+
+Component.prototype.toVDOMS = function toVDOMS(component) {
+    var obs,
+        children = component.children,
+        childObservables;
+
+    if (component instanceof Observable) {
+        obs = component.switchMap(toVDOMS);
+
+    } else if (children && children.length > 0) {
+
+        childObservables = children.map(toVDOMS);
+
+        childObservables.push(function() {
+            var latestChildren = toArray(arguments);
+            component.children = latestChildren;
+            return component;
+        });
+
+        obs = Observable.combineLatest.apply(Observable, childObservables);
+
+    } else {
+        obs = ObservableOfSync(component);
+    }
+
+    return obs;
+};
+
+Component.prototype.mount = function() {
+    this.mounted.onNext(true);
+};
+
+Component.prototype.unmount = function() {
+    this.mounted.onNext(false);
+};
+
+Component.prototype._subscribe = function() {
+
+    if (!this.vdoms) {
+        this.vdoms = this.toVDOMS(this.states.map(this.render));
+    }
+
+    return this.vdoms.subscribe.apply(this.vdoms, arguments);
+};
+
+module.exports = Component;
--- test/async.js
+++ test/async.js
@@ -1,12 +1,13 @@
-var elem = require('virtual-dom/h');
-var diff = require('virtual-dom/diff');
-var patch = require('virtual-dom/patch');
-var create = require('virtual-dom/create-element');
+var elem = require('../h');
+var diff = require('../diff');
+var patch = require('../patch');
+var create = require('../create-element');
+var Component = require('../Component');
 
 var Rx = require('rx');
 var Observable = Rx.Observable;
 
-var ObservableOfSync = Observable.ofWithScheduler.bind(Observable, Rx.Scheduler.immediate);
+// ---- Utils ----
 
 function eq(equalToVal) {
     return function(val) {
@@ -14,148 +15,16 @@ function eq(equalToVal) {
     };
 }
 
-function toArray(arrayLike) {
-    return Array.prototype.slice.call(arrayLike, 0);
-}
+var enterKeyDowns = Rx.Observable.fromEvent(document.body, "keydown").
+    pluck("keyCode").
+    where(eq(13));
 
-Rx.Observable.prototype.replayLast = function() {
-    var self = this,
-    last;
-
-    return Observable.create(function(observer) {
-        if (last !== undefined) {
-            observer.onNext(last);
-        }
-
-        return self.subscribe(
-            function onNext(value) {
-                last = value;
-                observer.onNext(value);
-            },
-            function onError(e) {
-                last = null;
-                observer.onError(e);
-            },
-            function () {
-                last = null;
-                observer.onCompleted();
-            });
-        });
-}
-
-function Component(state) {
-    this._states = new Rx.Subject();
-
-    this.mounted = new Rx.BehaviorSubject(false);
-    this.states = this._states;//.distinctUntilChanged();
-
-    this._subscribe = this.subscribe;
-}
-
-Component._cache = {};
-
-Component.create = function(proto) {
-
-    var component = function(state) {
-
-        var comp;
-
-        if (state && state.guid) {
-            comp = Component._cache[state.guid];
-        }
-
-        if (!comp) {
-            if (!(this instanceof component)) {
-                comp = new component(state);
-            } else {
-                Component.call(this, state);
-
-                if (state) {
-                    Component._cache[state.guid] = this;
-                }
-
-                comp = this;
-            }
-        } else {
-            comp.setState(state);
-        }
-
-        return comp;
-    };
-
-    component.prototype = Object.create(Component.prototype);
-    component.prototype.constructor = component;
-
-    for (var o in proto) {
-        component.prototype[o] = proto[o];
-    }
-
-    return component;
-}
-
-Component.prototype = Object.create(Observable.prototype);
-Component.prototype.constructor = Component;
-
-Component.prototype.render = function(state) {
-    return null;
-};
-
-Component.prototype.setState = function(state) {
-    this._states.onNext(state);
-};
-
-Component.prototype.toVDOMS = function toVDOMS(component) {
-    var obs,
-        children = (component.children),
-        obsChildren;
-
-    if (component instanceof Observable) {
-        obs = component.switchMap(toVDOMS);
-
-    } else if (children && children.length > 0) {
-
-        obsChildren = children.map(toVDOMS);
-
-        obsChildren.push(function combineLatestSelector() {
-            var latestChildren = toArray(arguments);
-            component.children = latestChildren;
-            return component;
-        });
-
-        obs = Observable.combineLatest.apply(Observable, obsChildren);
-
-    } else {
-        obs = ObservableOfSync(component);
-    }
-
-    return obs;
-};
-
-Component.prototype.mount = function() {
-    this.mounted.onNext(true);
-};
-
-Component.prototype.unmount = function() {
-    this.mounted.onNext(false);
-};
-
-Component.prototype.subscribe = function() {
-
-    if (!this.vdoms) {
-        // this.vdoms = new Rx.ReplaySubject(1);
-        // this.toVDOMS(this.states.map(this.render)).subscribe(this.vdoms);
-        //
-        // this.vdoms = this.states.map(this.render).replayLast();
-
-        this.vdoms = this.toVDOMS(this.states.map(this.render));
-    }
-
-    return this.vdoms.subscribe.apply(this.vdoms, arguments);
-};
+// ---- Custom Components ----
 
 var Label = Component.create({
 
     render: function(state) {
+
         var vdom = elem('div', {
             id: state.guid,
             style: {
@@ -164,7 +33,7 @@ var Label = Component.create({
                 padding: "5px",
                 display: "inline-block"
             }
-        }, state.text + "");
+        }, String(state.text));
 
         return vdom;
     }
@@ -174,6 +43,7 @@ var Label = Component.create({
 var Root = Component.create({
 
     render: function(state) {
+
         var vdom = elem("div", null, [
             Label({guid: "a", text: state.model.a}),
             Label({guid: "b", text: state.model.b})
@@ -184,41 +54,36 @@ var Root = Component.create({
 
 });
 
-var appRoot;
-var container = makeContainer();
+// ---- App Code ----
 
-var root = Root({guid:"root", model: {i: -1, a: -1, b: -1}}).publish().refCount();
+var root = Root({guid:"root"});
+var rootVDOMs = root.publish().refCount();
+var rootElem;
 
-root.take(1).forEach(function(vdom) {
-    appRoot = create(vdom);
-    container.appendChild(appRoot);
-});
+function initialRender(VDOM) {
+    var container = makeContainer();
 
-root.pairwise().
-    forEach(
-        function(pair) {
-            var patches = diff(pair[0], pair[1]);
-            appRoot = patch(appRoot, patches);
-        },
-        function() {
-            console.log("error");
-        },
-        function() {
-            console.log("complete");
-        });
+    rootElem = create(VDOM);
+    container.appendChild(rootElem);
+}
 
-Rx.Observable.fromEvent(document.body, "keydown").
-    doAction(function(e) {
-        e.preventDefault();
-    }).
-    pluck("keyCode").
-    where(eq(13)).
-    scan({
-        i: 0,
-        a: 0,
-        b: 0
-    }, function(prev, curr) {
+function incrementalRender(VDOMPair) {
+    var oldVDOM = VDOMPair[0];
+    var newVDOM = VDOMPair[1];
+    var patches = diff(oldVDOM, newVDOM);
 
+    rootElem = patch(rootElem, patches);
+}
+
+function setRootState(model) {
+    root.setState({guid:"root", model: model});
+}
+
+rootVDOMs.take(1).forEach(initialRender);
+rootVDOMs.pairwise().forEach(incrementalRender);
+
+enterKeyDowns.
+    scan({i: 0, a: 0, b: 0}, function(prev) {
         if (++prev.i % 2) {
             prev.a++;
         } else {
@@ -227,9 +92,11 @@ Rx.Observable.fromEvent(document.body, "keydown").
 
         return prev;
     }).
-    subscribe(function(model) {
-        Component._cache["root"].setState({guid:"root", model: model});
-    });
+    forEach(setRootState);
+
+// ---------- DOM Fluff ----------
+
+document.body.appendChild(document.createTextNode("Press Enter..."));
 
 function makeContainer() {
 
